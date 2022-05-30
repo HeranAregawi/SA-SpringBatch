aab3e0cf2df0722a8551b3734eb0f30d963b4b60
Heran Aregawi <heranaregawi@gmail.com>
2022-05-29 20:59:22 -0500

    docker-compose fixed and readme updated

--- Documents/MIU_Cources/SA/MiniProject-1/batch-processing-v1/src/main/java/com/miniproject/batchprocessingv2/batchconfig/springbatchconfig/SpringBatchConfig.java
+++ Documents/MIU_Cources/SA/MiniProject-1/batch-processing-v1/src/main/java/com/miniproject/batchprocessingv2/batchconfig/springbatchconfig/SpringBatchConfig.java
@@ -16,6 +16,7 @@ import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
@@ -35,7 +36,7 @@ public class SpringBatchConfig {
     @Bean
     public FlatFileItemReader<Student> reader() {
         FlatFileItemReader<Student> itemReader = new FlatFileItemReader<>();
-        itemReader.setResource(new FileSystemResource("src/main/resources/students.csv"));
+        itemReader.setResource(new ClassPathResource("students.csv"));
         itemReader.setName("csvReader");
         itemReader.setLinesToSkip(1);
         itemReader.setLineMapper(lineMapper());
